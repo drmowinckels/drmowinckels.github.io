32ea7b321e78853a838a43a66a27a9f00ad00511
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-04-01 19:42:06 +0200

    fix bluesky posting, and shorten url

--- .github/scripts/announce.R
+++ .github/scripts/announce.R
@@ -16,6 +16,25 @@ if (length(post) == 0) {
   post <- post[1]
 }
 
+short_url <- function(uri) {
+  resp <- httr2::request("http://v.gd/create.php") |>
+    httr2::req_url_query(
+      format = "json",
+      url = uri
+    ) |>
+    httr2::req_perform() |>
+    httr2::resp_body_string() |>
+    jsonlite::fromJSON()
+  resp$shorturl
+}
+
+strlength <- function(x) {
+  strsplit(x, "") |>
+    unlist() |>
+    length()
+}
+
+
 source(here::here(".github/scripts/linkedin.R"))
 source(here::here(".github/scripts/kit_newsletter.R"))
 
@@ -37,6 +56,7 @@ frontmatter <- rmarkdown::yaml_front_matter(post)
 # fix tags
 tags <- paste0("#", frontmatter$tags)
 tags <- sub("^#r$", "#rstats", tags, ignore.case = TRUE)
+tags <- sub(" ", "", tags, ignore.case = TRUE)
 tags <- paste(tags, collapse = " ")
 
 # build URL
@@ -44,103 +64,21 @@ uri <- sprintf(
   "https://drmowinckels.io/blog/%s/%s",
   basename(dirname(dirname(post))),
   frontmatter$slug
-)
+) |>
+  short_url()
 
+# fmt: skip
 emojis <- c(
-  "🦄",
-  "🦜",
-  "🦣",
-  "🦥",
-  "🦦",
-  "🦧",
-  "🦨",
-  "🦩",
-  "🦪",
-  "🦫",
-  "🦬",
-  "🦭",
-  "🦮",
-  "🦯",
-  "🦰",
-  "🦱",
-  "🦲",
-  "🦳",
-  "🦴",
-  "🦵",
-  "🦶",
-  "🦷",
-  "🦸",
-  "🦹",
-  "🦺",
-  "🦻",
-  "🦼",
-  "🦽",
-  "🦾",
-  "🦿",
-  "🧀",
-  "🧁",
-  "🧂",
-  "🧃",
-  "🧄",
-  "🧅",
-  "🧆",
-  "🧇",
-  "🧈",
-  "🧉",
-  "🧊",
-  "🧋",
-  "🧌",
-  "🧍",
-  "🧎",
-  "🧏",
-  "🧐",
-  "🧑",
-  "🧒",
-  "🧓",
-  "🧔",
-  "🧕",
-  "🧖",
-  "🧗",
-  "🧘",
-  "🧙",
-  "🧚",
-  "🧛",
-  "🧜",
-  "🧝",
-  "🧞",
-  "🧟",
-  "🧠",
-  "🧡",
-  "🧢",
-  "🧣",
-  "🧤",
-  "🧥",
-  "🧦",
-  "🧧",
-  "🧨",
-  "🧩",
-  "🧪",
-  "🧫",
-  "🧬",
-  "🧭",
-  "🧮",
-  "🧯",
-  "🧰",
-  "🧱",
-  "🧲",
-  "🧳",
-  "🧴",
-  "🧵",
-  "🧶",
-  "🧷",
-  "🧸",
-  "🧹",
-  "🧺",
-  "🧻",
-  "🧼",
-  "🧽",
-  "🧾",
-  "🧿"
+  "🦄", "🦜", "🦣", "🦥", "🦦", "🦧", "🦨", "🦩", "🦪", "🦫", 
+  "🦬", "🦭", "🦮", "🦯", "🦰", "🦱", "🦲", "🦳", "🦴", "🦵", 
+  "🦶", "🦷", "🦸", "🦹", "🦺", "🦻", "🦼", "🦽", "🦾", "🦿", 
+  "🧀", "🧁", "🧂", "🧃", "🧄", "🧅", "🧆", "🧇", "🧈", "🧉", 
+  "🧊", "🧋", "🧌", "🧍", "🧎", "🧏", "🧐", "🧑", "🧒", "🧓", 
+  "🧔", "🧕", "🧖", "🧗", "🧘", "🧙", "🧚", "🧛", "🧜", "🧝", 
+  "🧞", "🧟", "🧠", "🧡", "🧢", "🧣", "🧤", "🧥", "🧦", "🧧", 
+  "🧨", "🧩", "🧪", "🧫", "🧬", "🧭", "🧮", "🧯", "🧰", "🧱", 
+  "🧲", "🧳", "🧴", "🧵", "🧶", "🧷", "🧸", "🧹", "🧺", "🧻", 
+  "🧼", "🧽", "🧾", "🧿"
 )
 emoji <- sample(emojis, 1)
 
@@ -148,10 +86,9 @@ image <- here::here(dirname(post), frontmatter$image)
 
 # Post to Bluesky
 bskyr::bs_post(
-  text = substr(create_message(frontmatter$seo), 1, 250),
+  text = substr(create_message(frontmatter$seo), 1, (300 - strlength(uri))),
   images = image,
-  images_alt = frontmatter$image_alt,
-  langs = "US-en"
+  images_alt = frontmatter$image_alt
 )
 
 
