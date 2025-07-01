1e9349c541c921ad52d1bd6f3ae8b7aeeb627201
Athanasia Monika Mowinckel <a.m.mowinckel@psykologi.uio.no>
2025-07-01 21:54:56 +0200

    output links

--- .github/scripts/announce.R
+++ .github/scripts/announce.R
@@ -72,7 +72,7 @@ image <- here::here(
   )
 
 # Post to Bluesky
-bskyr::bs_post(
+bpst <- bskyr::bs_post(
   text = substr(
     create_message(frontmatter$seo, uri),
     1,
@@ -81,25 +81,28 @@ bskyr::bs_post(
   images = image,
   images_alt = frontmatter$image_alt
 )
+bskurl <- paste0("https://bsky.app/profile/drmowinckels.io/post/", bpst$cid)
+cli::cli_alert_info("Bluesky posted at {.url  {bskurl}}")
 
 
 # Post to LinkedIn
-li_post_write(
+lipst <- li_post_write(
   author = li_urn_me(),
   image_alt = frontmatter$image_alt,
   image = image,
   text = create_message(frontmatter$summary, url)
 )
+cli::cli_alert_info("Bluesky posted at {.url  {bskurl}}")
+
 
 # Post to Mastodon
-rtoot::post_toot(
+toot <- rtoot::post_toot(
   status = create_message(frontmatter$seo, uri),
   media = image,
   alt_text = frontmatter$image_alt,
   visibility = "public",
   language = "US-en"
 )
-
 
 # Send Newsletter
 send_newsletter(frontmatter, url)
